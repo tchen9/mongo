7134b68872bc51accd65d9f748a5caa8088dfcda
Tina <tchen9@stuy.edu>
2018-02-25 16:06:45 -0500

    started pokemon

---
+++ pokemon.py
@@ -0,0 +1,31 @@
+'''
+
+Pokemon Types
+
+Description of Contents: Includes names, generations, and damage relations of pokemons with urls
+
+Hyperlink: https://pokeapi.co/api/v2/type/1/
+
+Import Mechanism: 
+
+'''
+
+import pymongo
+import json
+
+connection = pymongo.MongoClient('homer.stuy.edu')
+db = connection['']
+collection = db['']
+
+'''
+def findname(name):
+
+def findgeneration(generation):
+
+def finddamage(damagerelations):
+
+def findindices(indices):
+
+def findindex(index):
+
+'''
